01a7b33943a7f471bf5789c6897b07e2256c5746
vozfrezz <tuekhac@gmail.com>
2023-04-26 23:04:35 +0700

    fix bug

--- src/assign1.c
+++ src/assign1.c
@@ -5,6 +5,7 @@
 float coords[6], altitude[3], medians[3], triangle_info[6]; // Coordinate
 float angleA, angleB, angleC, AB, BC, AC;
 float Ah, Bh, Ch, mA, mB, mC, p, area;
+float rightAngle = 90;
 float Pi = 3.1415926535897932384626433;
 
 // Function prototypes
@@ -23,7 +24,7 @@ void decode_triangle();
 int main() {
   input_coords();
   while (is_triangle() == 0) {
-    printf("\nVui long nhap lai toa do:");
+    printf("\nEnter the coordinates again.");
     input_coords();
   };
   side_triangle();
@@ -41,15 +42,15 @@ void input_coords() {
   char point = 'A'; // Point
   int i;
   for (i = 0; i < (sizeof(coords) / sizeof(coords[0])); i += 2) {
-    printf("\nNhap toa do diem %c:\n", point);
+    printf("\nEnter the coordinates of point %c:\n", point);
     scanf("%f%f", &coords[i], &coords[i + 1]);
     printf("\n%cx :%0.2f\n\n%cy :%0.2f\n", point, coords[i], point,
            coords[i + 1]);
     point++;
   };
-  printf("\nToa do diem A da nhap: A(%.2f,%.2f)\n", coords[0], coords[1]);
-  printf("Toa do diem B da nhap: B(%.2f,%.2f)\n", coords[2], coords[3]);
-  printf("Toa do diem C da nhap: C(%.2f,%.2f)\n", coords[4], coords[5]);
+  printf("\nThe coordinates of point A: A(%.2f,%.2f)\n", coords[0], coords[1]);
+  printf("The coordinates of point B: B(%.2f,%.2f)\n", coords[2], coords[3]);
+  printf("The coordinates of point C: C(%.2f,%.2f)\n", coords[4], coords[5]);
 };
 
 // Function to check if the entered coordinates form a triangle
@@ -68,20 +69,20 @@ int is_triangle() {
   AC = triangle_info[2];
   // Check if the sum of two sides is greater than the third side
   if (BC + AC <= AB || BC + AB <= AC || AC + AB <= BC) {
-    printf("\n3 diem A, B,C khong tao thanh tam giac\n");
+    printf("\nABC is not a triangle\n");
     return 0;
   } else {
-    printf("\nToa do 3 diem nhap vao tao thanh tam giac\n");
+    printf("\nABC is a triangle\n");
     return 1;
   };
 };
 
 // Function to print, calculate side lengths and angles
 void side_triangle() {
-  printf("\n1. So do co ban cua tam giac:\n");
-  printf("\nChieu dai canh BC: %.2f", BC);
-  printf("\nChieu dai canh AC: %.2f", AC);
-  printf("\nChieu dai canh AB: %.2f", AB);
+  printf("\n1. The basic diagram of triangle:\n");
+  printf("\nLength of side BC: %.2f", BC);
+  printf("\nLength of side AC: %.2f", AC);
+  printf("\nLength of side AB: %.2f", AB);
   // Calculate the angles using the cosine rule
   triangle_info[3] =
       acos((pow(AC, 2) + pow(AB, 2) - pow(BC, 2)) / (2 * AC * AB));
@@ -93,15 +94,15 @@ void side_triangle() {
   angleA = triangle_info[3] * 180 / Pi;
   angleB = triangle_info[4] * 180 / Pi;
   angleC = triangle_info[5] * 180 / Pi;
-  printf("\nGoc A co gia tri la: %0.2f", angleA);
-  printf("\nGoc B co gia tri la: %0.2f", angleB);
-  printf("\nGoc C co gia tri la: %0.2f\n", angleC);
+  printf("\nAngle A equal: %0.2f", angleA);
+  printf("\nAngle B equal: %0.2f", angleB);
+  printf("\nAngle C equal: %0.2f\n", angleC);
 };
 
 // Function to classify the triangle
 void classify_triangle() {
-  if (AB == BC == AC) {
-    printf("\nACB la tam giac deu");
+  if (AB == BC && BC == AC) {
+    printf("\nACB is equilateral triangle");
   } else if (AB == BC || AB == AC || BC == AC) {
     is_righttriangle();
   } else {
@@ -111,22 +112,31 @@ void classify_triangle() {
 
 // Function to check if the triangle is right-angled
 void is_righttriangle() {
-  if (angleA == 90) {
-    printf(" \ntam giac vuong can ta B");
-  } else if (angleB == 90) {
-    printf(" \ntam giac vuong can ta A");
-  } else if (angleC == 90) {
-    printf(" \btam giac vuong can ta C");
+  if (fabs(angleA - 90) < 0.01) {
+    printf(" \nABC is an isosceles right triangle at vertex A.");
+  } else if (fabs(angleB - 90) < 0.01) {
+    printf(" \nABC is an isosceles right triangle at vertex B.");
+  } else if (fabs(angleC - 90) < 0.01) {
+    printf(" \nABC is an isosceles right triangle at vertex C.");
   } else {
     if (AB == BC) {
-      (angleB > 90) ? printf("\nTam giac ACB can va tu tai B")
-                    : printf("\nTam giac ACB can tai B");
+      if (angleB > 90) {
+        printf("\nABC is an isosceles triangle and obtuse at B.");
+      } else {
+        printf("\nABC is an isosceles triangle at B.");
+      }
     } else if (AB == AC) {
-      (angleA > 90) ? printf("\nTam giac ACB can va tu tai A")
-                    : printf("\nTam giac ACB can tai A");
+      if (angleA > 90) {
+        printf("\nABC is an isosceles triangle and obtuse at A.");
+      } else {
+        printf("\nABC is an isosceles triangle at A.");
+      }
     } else if (BC == AC) {
-      (angleC > 90) ? printf("\nTam giac ACB can va tu tai C")
-                    : printf("\nTam giac ACB can tai C");
+      if (angleC > 90) {
+        printf("\nABC is an isosceles triangle and obtuse at C.");
+      } else {
+        printf("\nABC is an isosceles triangle at C.");
+      }
     };
   };
 };
@@ -134,13 +144,19 @@ void is_righttriangle() {
 // Function to check if the triangle is oblique
 void normal_triangle() {
   if (angleB > 90) {
-    printf("\nTam giac ACB nhon va tu tai B");
+    printf("\nACB is an obtuse triangle at B.");
   } else if (angleA > 90) {
-    printf("\nTam giac ACB nhon va tu tai A");
+    printf("\nACB is an obtuse triangle at A.");
   } else if (angleC > 90) {
-    printf("\nTam giac ACB nhon va tu tai C");
+    printf("\nACB is an obtuse triangle at C.");
+  } else if (fabs(angleA - 90) < 0.01) {
+    printf("\nACB is a right triangle at A.");
+  } else if (fabs(angleB - 90) < 0.01) {
+    printf("\nACB is a right triangle at B.");
+  } else if (fabs(angleC - 90) < 0.01) {
+    printf("\nACB is a right triangle at C.");
   } else {
-    printf("\nTam giac ACB nhon");
+    printf("\nACB is an acute triangle.");
   };
 };
 
@@ -148,7 +164,7 @@ void normal_triangle() {
 void caculate_area() {
   p = (AB + BC + AC) / 2;
   area = sqrt(p * (p - BC) * (p - AC) * (p - AB));
-  printf("\n\n2. Dien tich tam giac ACB la: %0.2f\n", area);
+  printf("\n\n2. The area of triangle ABC equal: %0.2f\n", area);
 };
 
 // Function to calculate the altitudes of the triangle (S = a.h/2, so h = 2S/a)
@@ -159,10 +175,10 @@ void altitudes_caculate() {
   altitude[0] = Ah;
   altitude[1] = Bh;
   altitude[2] = Ch;
-  printf("\n3. So do nang cao tam giac ACB:\n");
-  printf("\nDuong cao tu dinh A la:%0.2f", Ah);
-  printf("\nDuong cao tu dinh B la:%0.2f", Bh);
-  printf("\nDuong cao tu dinh C la:%0.2f", Ch);
+  printf("\n 3.Advanced diagram of a triangle ACB:\n");
+  printf("\nAltitude from vertex A:%0.2f", Ah);
+  printf("\nAltitude from vertex B:%0.2f", Bh);
+  printf("\nAltitude from vertex C:%0.2f", Ch);
 };
 
 // Function to calculate the medians
@@ -174,16 +190,16 @@ void medians_caculate() {
   medians[1] = mB;
   medians[2] = mC;
 
-  printf("\nDo dai duong trung tuyen tu dinh A: %f", mA);
-  printf("\nDo dai duong trung tuyen tu dinh B: %f", mB);
-  printf("\nDo dai duong trung tuyen tu dinh C: %f\n", mC);
+  printf("\nMedian lengths from vertex A: %0.2f", mA);
+  printf("\nMedian lengths from vertex B: %0.2f", mB);
+  printf("\nMedian lengths from vertex C: %0.2f\n", mC);
 };
 
 // Function to find the centroid of the triangle
 void median_point_triangle() {
   float median_point_G[2] = {(coords[0] + coords[2] + coords[4]) / 3,
                              (coords[1] + coords[3] + coords[5]) / 3};
-  printf("\n4. Toa do diem dac biet cua tam giac ABC:\n");
+  printf("\n4. The special coordinates of triangle ACB:\n");
   printf("\nMedian point from A: M1(%.2f, "
          "%.2f)\n-------------------------------------------------",
          median_point_G[0], median_point_G[1]);
@@ -194,7 +210,7 @@ void median_point_triangle() {
 void decode_triangle() {
   input_coords();
   while (is_triangle() == 0) {
-    printf("\nVui long nhap lai toa do:");
+    printf("\nEnter the coordinates again:");
     input_coords();
   };
   side_triangle();
